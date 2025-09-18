432f729d7c9ad6269f29d8f2ffdbe88f9aa5512a
Eliot Collombet <eliot.collombet@gmail.com>
2025-09-18 18:15:57 +0200

    Correction

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,62 +1,7 @@
-'use client';
-
 import SignoutButton from "@/components/login/signout-button";
 import getUsername from "@/components/login/get-username";
 
-import { useRef, useState } from 'react';
-import type { PutBlobResult } from '@vercel/blob';
-import { upload } from '@vercel/blob/client';
-
-function UploadSection() {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [uploading, setUploading] = useState(false);
-    const [lastBlob, setLastBlob] = useState<PutBlobResult | null>(null);
-    const [error, setError] = useState<string | null>(null);
-
-    async function onSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        setError(null);
-        setUploading(true);
-        try {
-            const file = inputRef.current?.files?.[0];
-            if (!file) throw new Error('Aucun fichier sélectionné');
-
-            const res = await upload(`images/${file.name}`, file, {
-                access: 'public',                 // URL publique (idéale pour assets)
-                handleUploadUrl: '/api/blob/upload', // notre route qui signe l’upload
-                contentType: file.type,           // aide à sécuriser côté client aussi
-            });
-
-            setLastBlob(res);
-
-            if (inputRef.current) inputRef.current.value = '';
-        } catch (err: any) {
-            setError(err?.message ?? 'Erreur inconnue');
-        } finally {
-            setUploading(false);
-        }
-    }
-
-    return (
-        <section>
-            <form onSubmit={onSubmit}>
-                <input ref={inputRef} type="file" accept="image/svg" required />
-                <button type="submit" disabled={uploading} style={{ marginLeft: 8 }}>
-                {uploading ? 'Upload…' : 'Uploader'}
-                </button>
-            </form>
-
-            {error && <p style={{ color: 'crimson' }}>Erreur : {error}</p>}
-
-            {lastBlob && (
-                <section style={{ marginTop: 24 }}>
-                <p><strong>URL:</strong> <a href={lastBlob.url} target="_blank">{lastBlob.url}</a></p>
-                <img src={lastBlob.url} alt="Dernier upload" style={{ maxWidth: '100%', borderRadius: 12 }} />
-                </section>
-            )}
-        </section>
-    );
-}
+import UploadSection from "@/components/upload-form";
 
 export default function Page() {
     return (
@@ -67,6 +12,7 @@ export default function Page() {
                 <div className="bg-background-highlight text-accent px-4 py-2"><p>#242424</p></div>
             </div>
             <SignoutButton/>
+            <UploadSection/>
         </div>
     );
 }
