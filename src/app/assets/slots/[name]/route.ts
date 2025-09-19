dd29cfd8bf98bf5aa51f2bcff9004c1936686805
Eliot Collombet <eliot.collombet@gmail.com>
2025-09-19 21:43:28 +0200

    meilleure api

--- src/app/assets/slots/[name]/route.ts
+++ src/app/assets/slots/[name]/route.ts
@@ -1,19 +1,20 @@
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
 import { head } from "@vercel/blob";
 import { SLOTS } from "@/slots";
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { name: string } }
+    request: Request,
+    { params }: { params: Promise<{ name: string }> }
 ) {
+    const { name } = await params
+
     try {
-        if (!SLOTS.includes(params.name as any)) throw new Error('Slot invalide');
+            if (!SLOTS.includes(name as any)) throw new Error('Slot invalide');
 
-        const meta = await head(`slots/${params.name}`);
+            const meta = await head(`slots/${name}`);
 
-        return NextResponse.redirect(meta.url, 302);
-    } catch (e) {
-        return NextResponse.json({ error: 'Slot introuvable' }, { status: 404 });
-    }
+            return NextResponse.redirect(meta.url, 302);
+        } catch (e) {
+            return NextResponse.json({ error: 'Slot introuvable' }, { status: 404 });
+        }
 }
