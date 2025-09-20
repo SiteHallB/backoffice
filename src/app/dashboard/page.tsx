a04bd6231fa68fe05cbbd6a6fad0c0ab9dfb7e1b
Eliot Collombet <eliot.collombet@gmail.com>
2025-09-20 14:26:23 +0200

    style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
         <div className="w-full bg-background-base flex flex-col items-center gap-8 py-4 px-4">
             <div className="w-full flex flex-wrap justify-between items-center gap-4 py-2">
                 <p className="text-foreground-subdued">Bienvenue, {getUsername()}</p>
-                <p className="text-foreground-base">La mise à jour des photos sur le site et le backoffice peuvent mettre jusqu'à 60s, ne pas oublier de refresh</p>
-                <p className="text-foreground-base">Avant d'upload les photos ne pas oublier de mettre la couleur d'arrière plan :</p>
-                <div className="bg-background-highlight text-accent px-4 py-2"><p>#242424</p></div>
+                <p className="text-foreground-base">La mise à jour des photos sur le site et le backoffice peut prendre jusqu'à 60s, raffraichir la page pour voir la mise à jour.</p>
+                <p className="text-foreground-base">Avant d'upload les photos ne pas oublier de mettre la couleur d'arrière plan (pour que ce soit assortit au site):</p>
+                <div className="bg-accent px-4 py-2"><p className="text-background-highlight font-futuretense uppercase">#242424</p></div>
             </div>
             <SignoutButton/>
             {SLOTS.map((slot) => (
