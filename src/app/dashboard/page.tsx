dd273a40f08b03cd2200570fe42558924afc8285
Eliot Collombet <eliot.collombet@gmail.com>
2025-09-20 15:30:58 +0200

    orthographe

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             <div className="w-full flex flex-wrap justify-between items-center gap-4 py-2">
                 <p className="text-foreground-subdued">Bienvenue, {getUsername()}</p>
                 <p className="text-foreground-base">La mise à jour des plannings sur le site et le backoffice peut prendre jusqu'à 60s, raffraichir la page pour voir la mise à jour.</p>
-                <p className="text-foreground-base">Important : exporter les plannings au format .svg et mettre la couleur d'arrière plan (pour que ce soit assortit au site):</p>
+                <p className="text-foreground-base">Important : exporter les plannings au format .svg et mettre la couleur d'arrière plan (pour que ce soit assorti au site):</p>
                 <div className="bg-accent px-4 py-2"><p className="text-background-highlight font-futuretense uppercase">#242424</p></div>
             </div>
             <SignoutButton/>
